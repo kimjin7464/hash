998018772b3870ed8c735c30889d5ef83b58c9b2
kimjin7464 <kim3115@purdue.edu>
2021-06-15 18:03:29 +0900

    new file

--- src/marathon.java
+++ src/marathon.java
@@ -1,2 +1,27 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class marathon {
+    public static String solution(String[] participant, String[] completion) {
+        int temp = 0;
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>(participant.length);
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], i);
+            temp += map.get(participant[i]);
+       //     System.out.println(map.get(participant[i]));
+        }
+        //System.out.println(temp);
+        //System.out.println(map.get());
+        for (int i = 0; i < completion.length; i++) {
+            temp -= map.get(completion[i]);
+            map.remove(completion[i]);
+        }
+        return participant[temp];
+    }
+    public static void main(String[] args) {
+        String[] participant = new String[]{"leo", "kiki", "eden", "leo"};
+        String[] completion = new String[]{"eden", "kiki", "leo"};
+        solution(participant, completion);
+    }
 }
